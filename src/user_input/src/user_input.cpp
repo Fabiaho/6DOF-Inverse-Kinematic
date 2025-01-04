91bf9bb3f58e2026b52f9a5dc3130d79f347f143
chiara <chiara.babiak@gmail.com>
2025-01-04 18:54:46 +0100

    Set Up: User Input Node

---
+++ src/user_input/src/user_input.cpp
@@ -0,0 +1,11 @@
+// ***************** INCLUDES ******************** //
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
+#include "std_msgs/Float64MultiArray.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <curl/curl.h>
+
+// ***************** VARIABLES ******************** //
+// ML Model Constant
